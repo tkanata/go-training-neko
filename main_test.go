c38fb9b044bf8d33b4efef88935d99b43e57db79
tkanata <tkanata@netprotections.co.jp>
2023-05-12 14:43:53 +0900

    テストの修正

--- main_test.go
+++ main_test.go
@@ -1,16 +1,29 @@
 package main
 
-import "testing"
+import (
+	"reflect"
+	"testing"
 
-func Test_main(t *testing.T) {
+	"github.com/tkanata/go-training-neko/model"
+)
+
+func Test_generateNeko(t *testing.T) {
+	type args struct {
+		n     model.Neko
+		color string
+	}
 	tests := []struct {
 		name string
+		args args
+		want model.Neko
 	}{
 		{name: "しろ"},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			main()
+			if got := generateNeko(tt.args.n, tt.args.color); !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("generateNeko() = %v, want %v", got, tt.want)
+			}
 		})
 	}
 }
